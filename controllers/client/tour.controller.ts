abcf775705d2bfeffc908f3e36b4b6a933485363
minhhkhoa <khoanguyen.1881@gmail.com>
2024-11-06 22:34:54 +0700

    4: Danh sách tour theo danh mục

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,18 +1,53 @@
 import { Request, Response } from "express"
 
-import Tour from "../../models/tour.model";
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
 
 
-//-[get]: /tours/
+//-[get]: /tours/:slugCategory
 export const index = async (req: Request, res: Response) => {
-  //- select * from tours where status = 'active' and deleted = false
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true //-ve lai data dung la 1 arr gom cac obj
+
+  /*
+    SELECT tours.* , price * (1 - discount / 100) AS price_special
+    FROM tours
+    JOIN tours_categories ON tours.id = tours_categories.tour_id
+    JOIN categories ON tours_categories.category_id = categories.id
+    WHERE
+      categories.slug = 'du-lich-trong-nuoc'
+      AND categories.deleted = false
+      AND categories.status = 'active'
+      AND tours.deleted = false
+      AND tours.status = 'active';
+  */
+  const slugCategory = req.params.slugCategory
+
+  const tours = await sequelize.query(`
+    SELECT tours.* , ROUND(price * (1 - discount / 100), 0) AS price_special
+    FROM tours
+    JOIN tours_categories ON tours.id = tours_categories.tour_id
+    JOIN categories ON tours_categories.category_id = categories.id
+    WHERE
+      categories.slug = '${slugCategory}'
+      AND categories.deleted = false
+      AND categories.status = 'active'
+      AND tours.deleted = false
+      AND tours.status = 'active';  
+  `, {
+    //- dinh nghia kieu muon lay (tránh lồng dữ liệu trả ra)
+    type: QueryTypes.SELECT
   })
+
+  tours.forEach(item => {
+    if(item["images"]){
+      const images = JSON.parse(item["images"])
+      //-tao key moi chua anh dau tien
+      item['image'] = images[0]
+    }
+
+    //- chinh gia tien ve so
+    item["price_special"] = parseFloat(item["price_special"])
+  })
+
 
   res.render("client/pages/tours/index", {
     pageTitle: "Danh sách tour",
--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -4,7 +4,7 @@ import * as controller from "../../controllers/client/tour.controller"
 
 const router: Router = Router()
 
-router.get("/", controller.index)
+router.get("/:slugCategory", controller.index)
 
 
 export const tourRoutes: Router = router
