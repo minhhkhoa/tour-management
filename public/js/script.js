0c1d4c696c2d40fcd245fdafefc29c8666009ed8
minhhkhoa <khoanguyen.1881@gmail.com>
2024-11-07 10:32:54 +0700

    2.1: them tour vao gio hang (localStorage)

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,46 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 //-end lider tour detail
+
+
+
+//-start cart 
+
+//-ktra xem trong local storage da co cart hay chua, neu ch thi phai add vao
+const cart = localStorage.getItem("cart")
+if (!cart) { //-ch co
+  localStorage.setItem("cart", JSON.stringify([]))
+}
+
+//-them tour vao cart
+const formAddToCard = document.querySelector("[form-add-to-cart]")
+if (formAddToCard) {
+  formAddToCard.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const quantity = parseInt(event.target.elements.quantity.value)
+    const tourId = parseInt(formAddToCard.getAttribute("tour-id"))
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"))
+
+      //-check xem da co tour do chua de update quantity
+      const isExistTour = cart.findIndex(item => item.tourId == tourId)
+
+      if (isExistTour == -1) { //-neu ch ton tai ==> them moi
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        })
+      } else{ //-neu ton tai thi update quantity
+        cart[isExistTour].quantity = cart[isExistTour].quantity + quantity
+      }
+
+
+      //-update vao storgare
+      localStorage.setItem("cart", JSON.stringify(cart))
+    }
+  })
+}
+
+//-end cart
