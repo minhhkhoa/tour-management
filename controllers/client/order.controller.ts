655f96f24c86752ed1575efa0bf8dd6973772964
minhhkhoa <khoanguyen.1881@gmail.com>
2024-11-07 22:02:23 +0700

    5.2: do data len giao dien dat hang thanh cong

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -3,6 +3,7 @@ import Order from "../../models/order.model";
 import { generateOrderCode } from "../../helpers/generate";
 import Tour from "../../models/tour.model";
 import OrderItem from "../../models/order-item.model";
+import { Json } from "sequelize/types/utils";
 
 //-[post]: /order
 export const order = async (req: Request, res: Response) => {
@@ -39,7 +40,7 @@ export const order = async (req: Request, res: Response) => {
     const dataItem = {
       orderId: orderId,
       tourId: item.tourId,
-      quantity: item.quantity 
+      quantity: item.quantity
     }
 
     //-lay du lieu cua tour do
@@ -69,7 +70,55 @@ export const order = async (req: Request, res: Response) => {
 
 //-[get]: /order/success
 export const success = async (req: Request, res: Response) => {
+
+  //-lay order code
+  const orderCode = req.query.orderCode
+
+  //- lay ra thong tin don hang
+  const order = await Order.findOne({
+    where: {
+      code: orderCode,
+      deleted: false
+    },
+    raw: true
+  })
+
+  //- 1 don co the co nhieu tour
+  const ordersItem = await OrderItem.findAll({
+    where: {
+      orderId: order["id"]
+    },
+    raw: true
+  })
+
+  for (const item of ordersItem) {
+    item["price_special"] = item["price"] * (1 - item["discount"] / 100)
+
+    //- tinh tong tien cua 1 tour(1 dong)
+    item["total"] = item["price_special"] * item["quantity"]
+
+    const tourInfo = await Tour.findOne({
+      where: {
+        id: item["tourId"]
+      },
+      raw: true
+    })
+
+    //-add key
+    item["title"] = tourInfo["title"]
+    
+    item["slug"] = tourInfo["slug"]
+
+    item["image"] = JSON.parse(tourInfo["images"])[0]
+  }
+
+  //-tong tien cua tat ca 
+  order["total_price"] = ordersItem.reduce((sum, item) => sum + item["total"], 0)
+
+
   res.render("client/pages/order/success", {
-    pageTitle: "Đặt hàng thành công"
+    pageTitle: "Đặt hàng thành công",
+    order: order,
+    ordersItem: ordersItem
   })
 }
--- public/js/script.js
+++ public/js/script.js
@@ -41,10 +41,12 @@ const showMiniCart = () => {
   const cart = JSON.parse(localStorage.getItem("cart"))
 
   // -tinh toan
-  const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0)
-
-  const miniCart = document.querySelector("[mini-cart]")
-  miniCart.innerHTML = totalQuantity
+  if(cart){
+    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0)
+    
+    const miniCart = document.querySelector("[mini-cart]")
+    miniCart.innerHTML = totalQuantity
+  }
 }
 showMiniCart()
 //-end mini cart
